19cc6cdd27901ba36a6516ec678dc4c9ce902bac
Rigo Mejia <rmejia4209@gmail.com>
2025-07-02 09:34:05 -0500

    Added scaffolding for layout

--- 2048/src/App.tsx
+++ 2048/src/App.tsx
@@ -15,14 +15,24 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-row gap-4 items-center">
+      <div className="flex flex-row gap-4 items-center mb-8">
+        <h1>2048</h1>
         <StatBlock label={"Score"} value={gameState.score}/>
         <StatBlock label={"Best"} value={101}/>
+      </div>
+      <div className="flex flex-row gap-4 items-center mb-8">
+        <p>Game introduction here</p>
+        <button>New Game</button>
       </div>
       <Tiles
         gameState={gameState}
         changeGameState={changeGameState}
       />
+      <div className="flex flex-row gap-4 items-center mb-8">
+        <button>power up</button>
+        <button>power up</button>
+        <button>power up</button>
+      </div>
     </>
   )
 }
