5b2c7af1039cb7e817291d530f941934df3b0bcf
Rigo Mejia <rmejia4209@gmail.com>
2025-07-22 06:18:15 +0000

    random int function added to use rand_c for threaded execution

--- ai/utilities.c
+++ ai/utilities.c
@@ -5,6 +5,56 @@
 #include <stdint.h>
 #include <string.h>
 #include <stddef.h>
+#include <time.h>
+#include <omp.h>
+#include "utilities.h"
+
+
+#ifdef USE_RAND_R
+/**
+ * Returns a pointer to a seed based on thread number. Use's knuth's
+ * constant to better distribute the seeds.
+ */
+static unsigned int *get_thread_seed(void) {
+    static uint32_t seeds[128];  // support for 128 threads max
+    int tid = omp_get_thread_num();
+    if (tid > 127) {
+        fprintf(stderr, "Error: using more than 128 threads");
+        exit(1);
+    }
+
+    if (seeds[tid] == 0) {
+        seeds[tid] = (uint32_t)(time(NULL) ^ (tid * 2654435761u));  //here?
+    }
+    return &seeds[tid];
+}
+
+
+#endif
+
+int random_int(int min, int max) {
+
+    if (min > max) {
+        fprintf(stderr, "Min is greater than max...");
+        exit(1);
+    }
+
+    int rand_num;
+    static int seeded = 0;
+
+    #ifdef USE_RAND_R
+        rand_num = rand_r(get_thread_seed());
+    #else
+        if (!seeded) {
+            srand((unsigned int) time(NULL));
+            seeded = 1;
+        }
+        rand_num = rand();
+    #endif
+        
+        return min + rand_num % (max - min + 1);
+}
+
 
 
 void shuffle_array(void *array, size_t num_elements, size_t element_size) {
@@ -16,7 +66,7 @@ void shuffle_array(void *array, size_t num_elements, size_t element_size) {
     if (!tmp) exit(1);
 
     for (size_t i = num_elements - 1; i > 0; i--) {
-        size_t j = rand() % (i + 1);
+        size_t j = (size_t)random_int(0, i);
 
         memcpy(tmp, arr + i * element_size, element_size);
         memcpy(arr + i * element_size, arr + j * element_size, element_size);
@@ -24,6 +74,8 @@ void shuffle_array(void *array, size_t num_elements, size_t element_size) {
     }
     free(tmp);
 }
+
+
 
 uint64_t convert_hex_string_to_big_int(const char *hex_str) {
     uint64_t val = 0;
--- ai/utilities.h
+++ ai/utilities.h
@@ -8,5 +8,5 @@
 // exposed functions
 void shuffle_array(void *array, size_t num_elements, size_t element_size);
 uint64_t convert_hex_string_to_big_int(const char *hex_str);
-
+int random_int(int min, int max);
 #endif
